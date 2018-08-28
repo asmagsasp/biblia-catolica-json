7736b77626a700f87fa8a755a6341239c8323b5f
Abel Magalhaes - Tecnologia da Informação - OBER S/A <abel@ober.com.br>
2018-08-28 09:38:03 -0300

    Fix IP 0.0.0.0

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+﻿from flask import Flask, jsonify, request
 from bs4 import BeautifulSoup
 import os   
 import urllib.request as urllib2
@@ -1749,4 +1749,4 @@ if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     # Tem que ser 0.0.0.0 para rodar no Heroku
-    app.run(host='127.0.0.1', port=port)    
+    app.run(host='0.0.0.0', port=port)    
